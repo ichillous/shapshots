5897985eb6cfcf3ac59f013bff044e6796a03432
ichillous <isiah.chillous@gmail.com>
2020-08-17 12:00:32 -0400

    js template

---
+++ app.js
@@ -0,0 +1,7 @@
+const DOMAIN = 'https://api.pexels.com/v1'
+const API_KEY = '563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9'
+const BASE_URL = `${'api.pexels.com/v1'}?apikey=${'563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9'}`
+
+async function showPicture(name) {
+    const url = `api.pexels.com/v1/?apikey=563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9`
+}
