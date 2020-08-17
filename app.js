d35a9579c7939609855780f58776fc3dfb39b19a
ichillous <isiah.chillous@gmail.com>
2020-08-17 12:53:39 -0400

    fixed console error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DOMAIN = 'https://api.pexels.com/v1'
+const DOMAIN = 'https://api.pexels.com/v1/search?query=people'
 const API_KEY = '563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9'
 const BASE_URL = `${'api.pexels.com/v1'}?apikey=${''}`
 
