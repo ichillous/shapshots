e7530d3e5614f503be5297e9e7fa12b8ce633a37
ichillous <isiah.chillous@gmail.com>
2020-08-17 12:47:33 -0400

    test

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const DOMAIN = 'https://api.pexels.com/v1'
 const API_KEY = '563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9'
-const BASE_URL = `${'api.pexels.com/v1'}?apikey=${'563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9'}`
+const BASE_URL = `${'api.pexels.com/v1'}?apikey=${''}`
 
-async function showPicture(name) {
-    const url = `api.pexels.com/v1/?apikey=563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9`
+async function showPicture() {
+    const resp = await axios.get(DOMAIN, { headers: {"Authorization" : `Bearer ${API_KEY}`} });
+    console.log(resp)
 }
+showPicture()
