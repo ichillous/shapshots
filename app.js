9bc346e1a580928962de43b487272bb674f3ff48
ichillous <isiah.chillous@gmail.com>
2020-08-20 16:29:33 -0400

    added css

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const showPics = document.querySelector('.renderPics')
 const API_KEY = '563492ad6f917000010000015ccce792a03d407c9f4d32cc165e44d9'
 const BASE_URL = `https://api.pexels.com/v1/search?`
 
+
+//               button waits for click event
 button.addEventListener('click', (e) => {
     console.log(BASE_URL)
     
-    e.preventDefault()
+    e.preventDefault() //Allows page to keep information w/o refreshing
     getResults(input.value)
 })
 
@@ -16,12 +18,12 @@ let getResults = async (value) => {
     let results = await axios.get(`${BASE_URL}query=${value}&apikey=${API_KEY}`, { headers: {"Authorization" : `Bearer ${API_KEY}`}})
     console.log(results)
     console.log(results.data.photos) 
-    let photos = results.data.photos
-    for (i = 0; i < photos.length; i++) {
+    let photos = results.data.photos //storing data in photo variable
+    for (i = 0; i < photos.length; i++) { //looping through photos
         console.log(photos[i].src.original)
-        let img = document.createElement('img')
-        img.src = photos[i].src.small
-        showPics.appendChild(img)
+        let img = document.createElement('img') //creating img variable
+        img.src = photos[i].src.small // storing photos info into img variable
+        showPics.appendChild(img) //appending img var to showPics Div.
     }
 }
 
